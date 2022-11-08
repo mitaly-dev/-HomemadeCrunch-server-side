1267514e2fa44e0c7cd1758cb807cea7190f240f
mitaly-dev <mitalyy.dev@gmail.com>
2022-11-07 18:26:39 -0800

    connect to mongodb database and add services data

---
+++ Data/service.js
@@ -0,0 +1,44 @@
+[
+    {
+      "service": "cheeseburger with salad",
+      "image": "https://i.ibb.co/tpHYWfX/service1.jpg",
+      "rating": "5",
+      "price": "19",
+      "description": "A hamburger, or simply burger, is a food consisting of fillings—usually a patty of ground meat, typically beef—placed inside a sliced bun or bread roll. Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chilis; condiments such as ketchup, mustard, mayonnaise, relish, or a , often a variation of Thousand Island dressing; and are frequently placed on sesame seed buns. A hamburger patty topped with cheese is called a cheeseburger.The term burger can also be applied to the meat patty on its own, especially in the United Kingdom, where the term patty is rarely used, or the term can even refer simply to ground beef. Since the term hamburger usually implies beef, for clarity burger may be prefixed with the type of meat or meat substitute used, as in beef burger, turkey burger, bison burger, portobello burger, or veggie burger. In Australia and New Zealand, a piece of chicken breast on a bun is known as a chicken burger, which would generally not be considered to be a burger in the United States; Americans would generally call it a chicken sandwich, but in Australian English and New Zealand English a sandwich requires sliced bread (not a bun), so it would not be considered a sandwich."
+    },
+    {
+      "service": "chrimp & olive pizza",
+      "image": "https://i.ibb.co/5j9wdzY/service2.jpg",
+      "rating": "18",
+      "price": "10",
+      "description": "Pizza (Italian: [ˈpittsa], Neapolitan: [ˈpittsə]) is a dish of Italian origin consisting of a usually round, flat base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients (such as various types of sausage, anchovies, mushrooms, onions, olives, vegetables, meat, ham, etc.), which is then baked at a high temperature, traditionally in a wood-fired oven.[1] A small pizza is sometimes called a pizzetta. A person who makes pizza is known as a pizzaiolo.n Italy, pizza served in a restaurant is presented unsliced, and is eaten with the use of a knife and fork.[2][3] In casual settings, however, it is cut into wedges to be eaten while held in the hand."
+    },
+    {
+      "service": "fried rice",
+      "image": "https://i.ibb.co/f9kVxxh/service4.jpg",
+      "rating": "5",
+      "price": "7",
+      "description": "Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat. It is often eaten by itself or as an accompaniment to another dish. Fried rice is a popular component of East Asian, Southeast Asian and certain South Asian cuisines, as well as a staple national dish of Indonesia. As a homemade dish, fried rice is typically made with ingredients left over from other dishes, leading to countless variations. Fried rice first developed during the Sui Dynasty in China and as such all fried rice dishes can trace their origins to Chinese fried rice.ny varieties of fried rice have their own specific list of ingredients. In Greater China, common varieties include Yangzhou fried rice and Hokkien fried rice. Japanese chāhan is considered a Japanese Chinese dish, having derived from Chinese fried rice dishes. In Southeast Asia, similarly constructed Indonesian, Malaysian, and Singaporean nasi goreng and Thai khao phat are popular dishes. In the West, most restaurants catering to vegetarians have invented their own varieties of fried rice, including egg fried rice. Fried rice is also seen on the menus of American restaurants offering cuisines with no native tradition of the dish. "
+    },
+    {
+      "service": "crispy fried chicken",
+      "image": "https://i.ibb.co/bRGbppW/service5.png",
+      "rating": "5",
+      "price": "16",
+      "description": "Fried chicken, also known as Southern fried chicken, is a dish consisting of chicken pieces that have been coated with seasoned flour or batter and pan-fried, deep fried, pressure fried, or air fried. The breading adds a crisp coating or crust to the exterior of the chicken while retaining juices in the meat. Broiler chickens are most commonly used.The first dish known to have been deep fried was fritters, which were popular in the European Middle Ages. However, the Scottish were the first Europeans to deep fry their chicken in fat (though without seasoning). Meanwhile, many West African peoples had traditions of seasoned fried chicken (though battering and cooking the chicken in palm oil). Scottish frying techniques and West African seasoning techniques were combined by enslaved Africans and African Americans in the American South"
+    },
+    {
+      "service": "fish cutlet",
+      "image": "https://i.ibb.co/g4cbWkn/service3.jpg",
+      "rating": "15",
+      "price": "12",
+      "description": "Fish cutlets (or you can call them fish cakes or fish patties) made with fish, veggies and spices are coated in egg and breadcrumbs, and deep fried to golden perfection. These Kerala style cutlets are traditionally served as starters or appetizers along with this simple onion salad."
+    },
+    {
+      "service": "hakka noodles",
+      "image": "https://i.ibb.co/gdRkZfT/service6.jpg",
+      "rating": "5",
+      "price": "8",
+      "description": "Hakka noodles are Indian-Chinese style noodles made with unleavened refined wheat flour. These are boiled al dente and then stir fried in a Chinese wok with oil, vegetables & soya sauce. Here hakka noodles refers to both the final dish and the type of noodles used. There are a lot of brands selling hakka noodles."
+    }
+  ]
